1fd7992a97e0c1a52adaf82cc5b43d4c05c25600
v_dengwei_dxm <v_dengwei@duxiaoman.com>
2021-11-23 20:57:19 +0800

    完成评论功能

--- comments/urls.py
+++ comments/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'comments'
 urlpatterns = [
-    path('comment/<int:post_pk>', views.comment, name='comment'),
+    path('comment/<int:post_id>', views.comment, name='comment'),
 ]
